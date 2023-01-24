6852e64fe7c9cab9aba2ab65db5140918388ed4d
Min Xu <min.m.xu@intel.com>
2023-01-24 08:30:59 +0800

    Use MySql instead of Sql Server
    The previous implementation uses Sql server. This patch use MySql
    and its version should be 8.0+.
    
    Signed-off-by: Min Xu <min.m.xu@intel.com>

--- Precise-Core/Precise.EntityFrameworkCore/EntityFrameworkCore/PreciseDbContextConfigurer.cs
+++ Precise-Core/Precise.EntityFrameworkCore/EntityFrameworkCore/PreciseDbContextConfigurer.cs
@@ -7,12 +7,14 @@ namespace Precise.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<PreciseDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            //builder.UseSqlServer(connectionString);
+            builder.UseMySql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<PreciseDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            //builder.UseSqlServer(connection);
+            builder.UseMySql(connection);
         }
     }
 }
